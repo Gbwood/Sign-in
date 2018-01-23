1d9dcb015d8fee6895ee13a16d1361fd13ac917e
John Austin Smith <jasmith1995@ksu.edu>
2018-01-23 14:52:39 -0600

    test

---
+++ LoginAssigment/LoginAssigment/LoginForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LoginAssigment
+{
+    partial class LoginUx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.UserLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserText = new System.Windows.Forms.TextBox();
+            this.PassText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(255, 6);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(75, 22);
+            this.LoginBtn.TabIndex = 0;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(255, 42);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 22);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Exit";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // UserLbl
+            // 
+            this.UserLbl.AutoSize = true;
+            this.UserLbl.Location = new System.Drawing.Point(12, 9);
+            this.UserLbl.Name = "UserLbl";
+            this.UserLbl.Size = new System.Drawing.Size(77, 17);
+            this.UserLbl.TabIndex = 2;
+            this.UserLbl.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // UserText
+            // 
+            this.UserText.Location = new System.Drawing.Point(95, 6);
+            this.UserText.Name = "UserText";
+            this.UserText.Size = new System.Drawing.Size(149, 22);
+            this.UserText.TabIndex = 4;
+            // 
+            // PassText
+            // 
+            this.PassText.Location = new System.Drawing.Point(95, 42);
+            this.PassText.Name = "PassText";
+            this.PassText.PasswordChar = '*';
+            this.PassText.Size = new System.Drawing.Size(149, 22);
+            this.PassText.TabIndex = 5;
+            // 
+            // LoginUx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 106);
+            this.Controls.Add(this.PassText);
+            this.Controls.Add(this.UserText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserLbl);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.LoginBtn);
+            this.Name = "LoginUx";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label UserLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UserText;
+        private System.Windows.Forms.TextBox PassText;
+    }
+}
+
---
+++ LoginAssigment/LoginAssigment/LoginForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginAssigment
+{
+    public partial class LoginUx : Form
+    {
+        public int count = 3;
+        public string pass = "";
+        public LoginUx()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if(UserText.Text == "user" && PassText.Text == "pass")
+            {
+                this.Hide();
+                MessageBox.Show("You have been authorized to proceed.");
+                MainForm mf = new MainForm();
+                mf.Show();
+            }
+            else if(count != 1)
+            {
+                count--;
+                MessageBox.Show("You have " + count + " more tries before your account is locked for 2 hours.");
+            }
+            else
+            {
+                MessageBox.Show("Your account has been locked for 2 hours.");
+                Close();
+            }
+        }
+
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
